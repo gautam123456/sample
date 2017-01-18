03e5f677f99bfaa60fb1e04109a018be06994469
ranjeetkumar_gautam@intuit.com <ranjeetkumar_gautam@intuit.com>
2017-01-18 22:42:33 +0530

    fixed header bug

--- src/components/ServicesList.js
+++ src/components/ServicesList.js
@@ -4,9 +4,16 @@
 import React from 'react';
 import ServiceMenu from './ServiceMenu';
 
+import Data from '../../data/items.json';
+import bookingDetails from '../../data/constants.json';
+
 export default class ServicesList extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  renderHeading(title, id) {
+    return <header key = { id } className = 's-heading full-width'>{ title }</header>
   }
 
   render() {
@@ -16,6 +23,7 @@ export default class ServicesList extends React.Component {
         {
           this.props.data.serviceCategoryList.map(function(title) {
             return <div>
+                      { then.renderHeading(title.name, then.props.service + '-' + title.id) }
                       { title.serviceItemList.map(function(list) {
                         let id = then.props.service + '-' + title.id + '-' + list.id;
                         return <ServiceMenu list = {list} count = { then.props.bookingDetails.services && then.props.bookingDetails.services[id] ? then.props.bookingDetails.services[id].count : 0 } key = { id } id = { id } bookingDetailsChanged = { then.props.bookingDetailsChanged.bind(this) }/>
