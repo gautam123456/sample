09e169bca79ac47e6bfd83186e1b0e4a8447bd32
Gautam, Ranjeet Kumar <RanjeetKumar_Gautam@intuit.com>
2017-04-02 11:51:52 +0530

    API changed

--- src/components/ActivityFooter.js
+++ src/components/ActivityFooter.js
@@ -10,17 +10,17 @@ export default class ActivityFooter extends React.Component {
     render() {
         return (
             <div className='a-footer col-xs-12'>
-                  <span className='col-xs-3 pad0 a-f'>
-                    <Link to = { '/' + this.props.back }>
-                        <i className='fa fa-arrow-circle-left'></i> &nbsp; Back
-                    </Link>
-                  </span>
-                  <span className='col-xs-6'></span>
-                  <span className='col-xs-3 pad0 a-f'>
-                      <Link to = { '/' + this.props.next }>Next &nbsp;
-                          <i className='fa fa-arrow-circle-right'></i>
-                      </Link>
-                  </span>
+              <span className='col-xs-3 pad0 a-f'>
+                <Link to = { '/' + this.props.back }>
+                    <i className='fa fa-arrow-circle-left'></i> &nbsp; Back
+                </Link>
+              </span>
+              <span className='col-xs-6'></span>
+              <span className='col-xs-3 pad0 a-f'>
+                  <Link to = { '/' + this.props.next }>Next &nbsp;
+                      <i className='fa fa-arrow-circle-right'></i>
+                  </Link>
+              </span>
             </div>
         )
     }
--- src/components/base/Shell.js
+++ src/components/base/Shell.js
@@ -15,22 +15,21 @@ export default class Login extends React.Component {
   render() {
     const self = this;
     return (
-      <div className='shell col-md-4 col-xs-12 col-md-offset-4'>
+      <div className='shell col-md-4 col-xs-12 col-md-offset-4 pad0'>
         <div className='shell-in'>
           <header className='head'>
             <div className='col-xs-12 icons'>
               <span className='col-xs-1 pull-right'>
                 <Link to = { '/' }>
-                  <i className='fa fa-times fa-lg'></i>
+                  &#215;
                 </Link>
               </span>
-              <span className='col-xs-1 pull-right'>
+              <span className='col-xs-2 pull-right'>
                 <Link to = { '/' }>
-                  <i className='fa fa-shopping-cart fa-lg'></i>
+                  <i className='fa fa-shopping-cart'></i>
                 </Link>
               </span>
             </div>
-            <div className='col-xs-12'></div>
           </header>
           <div className='body'>
 
