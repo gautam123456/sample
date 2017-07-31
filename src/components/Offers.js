cd52f46c7323c688077ff2385d12e139aa9c2dfd
Gautam, Ranjeet Kumar <RanjeetKumar_Gautam@intuit.com>
2017-07-31 22:29:58 +0530

    BF

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -45,11 +45,11 @@ export default class Offers extends React.Component {
     return (
       <div className = 'offer col-xs-10 col-xs-offset-1 pad0' key={offer.couponCode}>
         <div className= 'col-xs-12 header' >
-          <div className='col-xs-6 pad0'>Coupon Code: <strong id='code'>{offer.couponCode}</strong></div>
-          <div className='col-xs-6' style={{background: "url('../styles/assets/images/barcode.png') center no-repeat",backgroundSize: 'contain', height: 120}}></div>
+          <div className='col-xs-10 pad0'>Coupon Code: <strong id='code'>{offer.couponCode}</strong></div>
+          <div className='col-xs-5 col-xs-offset-7' style={{background: "url('../styles/assets/images/barcode.png') center no-repeat",backgroundSize: 'contain', height: 120}}></div>
         </div>
         <div className= 'col-xs-12 body'><strong>{offer.discount}%</strong> Off</div>
-        <div className= 'col-xs-4 pad0 pull-right' style={{fontSize: 8}}>Valid till : {offer.validthru || 'Limited Period'}</div>
+        <div className= 'col-xs-5 pad0 pull-right' style={{fontSize: 8, textAlign: 'right'}}>Valid till : {offer.validthru || 'Limited Period'}</div>
       </div>
     )
   }
